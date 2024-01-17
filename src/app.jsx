0d75c225eba2a80c43047d2d89fa4cc564556b71
Bree Armstrong <barmstrong@arccorp.com>
2024-01-17 16:57:06 -0500

    begin working to update the page to reflect current page

--- src/app.jsx
+++ src/app.jsx
@@ -1,44 +1,109 @@
-import React, { Component } from "react";
+import React from "react";
+
+import {
+  Stickynav,
+  ProductJumbo,
+  ProductText,
+  ProductCallout,
+  ProductLinks,
+} from "arccorp-vars";
 
 import { stories } from "./stories";
+import CustomerSuccessCard from "./components/CustomerSuccessCard";
 
 const customers = ["Agency", "Airlines", "Other Data Users"];
-let selectedCustomer = ""
-let filter = ""
+let selectedCustomer = "";
+let filter = "";
 
 function customerFilter() {
- if (selectedCustomer == "Agency"){
-    filter = stories.filter(story => story.type == "agency")
- }
- if (selectedCustomer == "Airlines"){
-    filter = stories.filter(story => story.type == "carrier")
- }
- if (selectedCustomer == "Other Data Users"){
-    filter = stories.filter(story => story.type == "edu" )
- }
- else {
+  if (selectedCustomer == "Agency") {
+    filter = stories.filter((story) => story.type == "agency");
+  }
+  if (selectedCustomer == "Airlines") {
+    filter = stories.filter((story) => story.type == "carrier");
+  }
+  if (selectedCustomer == "Other Data Users") {
+    filter = stories.filter((story) => story.type == "edu");
+  } else {
     filter = stories;
- }
+  }
+}
+
+//Thinking of making a product array to then search in and replace. 
+function replaceProduct() {
+  if (prdouct == "ARC Direct Connect") {
+    //replace the word with a link
+  }
 }
 
 function App() {
-    customerFilter();
+  customerFilter();
   return (
-    <>
-      <h1>Hey</h1>
-      <div>
-        {filter.map((story, i) => {
-            console.log(story)
-          return (
-            <div key={i}>
-              <div>{story.name}</div>
-              <div>{story.type}</div>
-              <p>{story.quote}</p>
+    <div className="arc-customer-succcess">
+      <Stickynav
+        className="bg-color-teal"
+        pageLink="#top"
+        title="Customer Success Stories"
+        stickyCTA="View Products"
+        stickyCTALink="https://www2.arccorp.com/products-participation/products/"
+      />
+      <ProductJumbo
+        className="customer-success-jumbo"
+        backgroundImage="https://www2.arccorp.com/globalassets/homepage/redesign/customersuccess-2x.jpg"
+        title={
+          <>
+            Customer <br />
+            Success Stories
+          </>
+        }
+        subtitle={
+          <>
+            Learn how customers are using{" "}
+            <span>
+              ARC’s products to drive efficiencies, increase revenue, and
+              improve their
+            </span>{" "}
+            businesses, as well as the industry as a whole.
+          </>
+        }
+        ctaTitle="Share Your Story"
+        ctaLink="http://www2.arccorp.com/products-participation/customer-success-stories#feedback-form"
+      />
+      <div className="success-story-section">
+        <div className="success-story-section-inner">
+          <div className="row no-gutters">
+            <div className="col-lg-12">
+              <div className="customer-card-filter">Filter:</div>
             </div>
-          );
-        })}
+          </div>
+          <div className="row no-gutters">
+            <div className="col-lg-9">
+              <div className="row">
+                <div className="col-md-12">
+                  {filter.map((story, i) => {
+                    console.log(story);
+                    return (
+                      <CustomerSuccessCard
+                        key={i}
+                        name={story.name}
+                        type={story.type != "carrier" ? story.type : "Airlines"}
+                        quote={story.quote}
+                        title={story.title}
+                        company={story.company}
+                        productLink={story.productLink}
+                        productName={story.productName}
+                        product={story.product}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3"></div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/CustomerSuccessCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function CustomerSuccessCard(props) {
+  return (
+    <div className="col-lg-12">
+      <div className="customer-success-card">
+        <div className="customer-success-card-inner">
+          <p className="customer-quote">"{props.quote}"</p>
+          <p>
+            <b className="customer-name">{props.name}</b> &nbsp; <em className="customer-title">{props.title}</em> &nbsp;
+            <span className="customer-company">{props.company}</span>
+          </p>
+          <a className="ctaBtn" href={props.producLink}>
+            {props.productName}
+          </a>
+        </div>
+        <div className="customer-success-card-tag">
+          {props.type} / {props.product}
+        </div>
+      </div>
+    </div>
+  );
+}
